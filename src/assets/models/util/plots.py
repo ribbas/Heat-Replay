fd153c769044fdb8d9632254e2947a0cce75e811
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-07-24 17:56:48 -0400

    models are pretty much finished

--- src/assets/models/util/plots.py
+++ src/assets/models/util/plots.py
@@ -1,4 +1,6 @@
+from matplotlib.patches import Patch
 import matplotlib.pyplot as plt
+from numpy import mean
 from seaborn import set as sns_set
 from seaborn import boxplot, pairplot
 
@@ -16,15 +18,46 @@ def boxplotify(df, feature, path, title, save=True):
 
     fig, ax = plt.subplots(figsize=(12, 5))
 
-    ax.set_title(title)
+    fig.suptitle(title, fontsize=20)
 
     boxplot(
         x=df['decade'], y=df[feature],
         hue=df['charted'],
-        linewidth=2, ax=ax, palette={0: 'r', 1: 'g'}
+        linewidth=2, ax=ax,
+        palette={0: 'r', 1: 'g'}
+    )
+
+    yes = Patch(color='g', label='Yes')
+    no = Patch(color='r', label='No')
+    plt.legend(
+        bbox_to_anchor=(1, 1), loc=2,
+        ncol=1, shadow=True, title="Charted",
+        handles=[yes, no]
     )
 
     if save:
         fig.savefig(staticDir.format(file=path))
 
     plt.show()
+
+
+def meanLine(df, feature):
+
+    year = set(df['year'])
+
+    # Plot the mean of the feature
+    x, y = zip(
+        *sorted(
+            (
+                xVal, mean(
+                    [
+                        yVal for av, yVal in zip(
+                            df['year'], df[feature]
+                        ) if xVal == av
+                    ]
+                )
+            ) for xVal in year
+        )
+    )
+
+    plt.plot(x, y, 'r-')
